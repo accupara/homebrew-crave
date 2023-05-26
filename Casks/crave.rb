70ca5ea3b179767b507cf4a60fa7a6ed5541e262
Ruchi Gupta <ruchi@accupara.com>
2023-05-26 10:21:05 +0530

    add version and correct the sha256

--- Casks/crave.rb
+++ Casks/crave.rb
@@ -1,10 +1,10 @@
 cask "crave" do
   version "0.2-6767"
-  sha256 "a792e5526e27d7149ec0569a98bdf507da7f001ce3cfc5bebd2e6d05de7c9911"
+  sha256 "a64134642a23a32c29d4440bfd9e5de8947c2a3c09ad8b931daa62e1b76cdda2"
 
-  url "https://github.com/accupara/crave/releases/download/0.2-6767/crave-0.2-6767-darwin-amd64.bin"
+  url "https://github.com/accupara/crave/releases/download/#{version}/crave-#{version}-darwin-amd64.bin"
   name "Crave"
   desc "A command line tool to running commands against Crave platform clusters."
   homepage "https://www.crave.io"
-  binary "crave-0.2-6767-darwin-amd64.bin", target: "crave"
+  binary "#{version}-darwin-amd64.bin", target: "crave"
 end

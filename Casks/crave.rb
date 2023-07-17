19f068aace7d36b39305f1102f061002ee434be4
Ruchi Gupta <ruchi@accupara.com>
2023-07-17 15:34:33 +0530

    update crave version

--- Casks/crave.rb
+++ Casks/crave.rb
@@ -1,6 +1,6 @@
 cask "crave" do
-  version "0.2-6772"
-  sha256 "639efe90bf1fd68a2dd10e50d6801ecbf493f72e9c6b122c972967431a1e69dd"
+  version "0.2-6816"
+  sha256 "57b1f125c9ff410945c7c508f3ce53c3457e77ca2a842433babc124722df696f"
 
   url "https://github.com/accupara/crave/releases/download/#{version}/crave-#{version}-darwin-amd64.bin"
   name "Crave"
